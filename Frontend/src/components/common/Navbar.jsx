3be53ca5a7078e34eb5ade4001f7c0465fb920a8
Rajan <rajanmasih154@gmail.com>
2024-06-15 23:39:03 +0530

    Add initial project files and configurations

---
+++ Frontend/src/components/common/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex justify-between p-5 bg-gray-200'>
+    <h1 className='font-bold'>Payment Apps</h1>
+    <p className='font-light'>Hello, User</p>
+    </div>
+  )
+}
+
+export default Navbar
