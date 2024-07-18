f36ef290d6d8d2b515e0af841048e12341f50280
Rajan <rajanmasih154@gmail.com>
2024-07-18 12:31:53 +0530

    Fixed: Database connection

--- Backend/index.js
+++ Backend/index.js
@@ -4,17 +4,19 @@ const MongoDb = require("./src/config/Database");
 const userRoute = require("./src/routes/User.routes");
 const cors = require("cors");
 
-app.use(cors({
+app.use(
+  cors({
     origin: "*",
-}));
+  })
+);
 app.use(express.json());
 
-MongoDb.then(
-    app.listen(3000, () => {
-        console.log("Server Started at 3000");
-    })
+MongoDb.then(() =>
+  app.listen(3000, () => {
+    console.log("Server Started at 3000");
+  })
 ).catch(() => {
-    console.error("Server Error");
-})
+  console.error("Server Error");
+});
 
-app.use("/api/v1/users", userRoute)
+app.use("/api/v1/users", userRoute);
--- Backend/src/config/Database.js
+++ Backend/src/config/Database.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const MongoDb = mongoose
   .connect("mongodb://localhost:27017/PAYTM")
-  .then(console.log("Database Connected !!"))
-  .catch(() => {
-    console.error("Database Error");
+  .then(() => console.log("Database Connected !!"))
+  .catch((err) => {
+    console.error("Database Error", err);
+    process.exit(1);
   });
 
 module.exports = MongoDb;
