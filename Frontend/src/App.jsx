2d7ab5c3f7833e3e5994d223e602632264153685
Rajan <rajanmasih154@gmail.com>
2024-08-02 01:27:50 +0530

    chore: Add React Router for routing and update main.jsx and App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Signup from "./components/Auth/Signup";
 import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
