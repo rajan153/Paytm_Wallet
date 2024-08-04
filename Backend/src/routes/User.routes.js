d6d32538b7efcf3c8aaf3a6024b4282babc7e56f
Rajan <rajanmasih154@gmail.com>
2024-08-05 02:35:31 +0530

    feat: Add route for getting users with authentication middleware

--- Backend/src/routes/User.routes.js
+++ Backend/src/routes/User.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { Signup, login } = require("../controllers/User.controller");
+const { Signup, login, getUsers } = require("../controllers/User.controller");
 const router = express.Router();
+const { authMiddleware } = require("../middleware/Auth.middleware");
 
 router.route("/regiters").post(Signup);
 router.route("/login").post(login);
+router.route("/getusers").get(authMiddleware, getUsers);
 
 module.exports = router;
