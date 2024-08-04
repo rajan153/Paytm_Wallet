b0303772f52c93795e0f494e55e7d6bda409ebb6
Rajan <rajanmasih154@gmail.com>
2024-08-05 02:35:53 +0530

    feat: Update landing page route to use Dashboard component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import Navbar from "./components/common/Navbar";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Dashboard />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
