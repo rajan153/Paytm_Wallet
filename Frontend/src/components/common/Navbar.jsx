12f9a0ecba32f759b7c9bd0a5cc9f486961dacb5
Rajan <rajanmasih154@gmail.com>
2024-08-05 02:36:36 +0530

    refactor: Improve Navbar component UI and add logout functionality

--- Frontend/src/components/common/Navbar.jsx
+++ Frontend/src/components/common/Navbar.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import Button from "../ui/Button";
+import { useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  const logoutHanlder = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
-    <div className="shadow h-14 flex justify-between">
+    <div className="shadow flex justify-between p-4 items-center">
       <div className="flex flex-col justify-center h-full ml-4">PayTM App</div>
       <div className="flex">
-        <div className="flex flex-col justify-center h-full mr-4">Hello</div>
-        <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-          <div className="flex flex-col justify-center h-full text-xl">U</div>
-        </div>
+        {token && <Button label={"Logout"} onClick={logoutHanlder} />}
       </div>
     </div>
   );
