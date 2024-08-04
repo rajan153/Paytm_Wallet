62a1f8f3f11683ab03d2ec58a6125f32529a138e
Rajan <rajanmasih154@gmail.com>
2024-08-05 02:36:28 +0530

    feat: Add login and signup functionality to Auth components

--- Frontend/src/components/Auth/Login.jsx
+++ Frontend/src/components/Auth/Login.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../ui/Heading";
 import Paragraph from "../ui/Paragraph";
 import Input from "../ui/Input";
 import Button from "../ui/Button";
 import BottomWarning from "../ui/BottomWarning";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onClick = async () => {
+    const response = await axios.post(
+      "http://localhost:3000/api/v1/users/login",
+      {
+        username,
+        password,
+      }
+    );
+    localStorage.setItem("token", response.data.token);
+    navigate("/");
+  };
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign in"} />
           <Paragraph label={"Enter your credentials to access your account"} />
-          <Input placeholder="harkirat@gmail.com" label={"Email"} />
-          <Input placeholder="123456" label={"Password"} />
+          <Input
+            placeholder="harkirat@gmail.com"
+            label={"Email"}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="123456"
+            label={"Password"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <div className="pt-4">
-            <Button label={"Sign in"} />
+            <Button onClick={onClick} label={"Sign in"} />
           </div>
           <BottomWarning
             label={"Don't have an account?"}
--- Frontend/src/components/Auth/Signup.jsx
+++ Frontend/src/components/Auth/Signup.jsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../ui/Heading";
 import Paragraph from "../ui/Paragraph";
 import Input from "../ui/Input";
 import Button from "../ui/Button";
 import BottomWarning from "../ui/BottomWarning";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const onClick = async () => {
+    await axios.post("http://localhost:3000/api/v1/users/regiters", {
+      username,
+      firstName,
+      lastName,
+      password,
+    });
+    navigate("/login");
+  };
   return (
     <div className="bg-slate-300 h-screen flex justify-center items-center">
       <div className="flex flex-col justify-center items-center bg-slate-100 rounded-xl shadow-xl">
         <Heading label={"Sign up"} />
         <Paragraph label={"Enter your infromation to create an account"} />
-        <Input placeholder="John" label={"First Name"} />
-        <Input placeholder="Doe" label={"Last Name"} />
-        <Input placeholder="abc@gmail.com" label={"Email"} />
-        <Input placeholder="123456" label={"Password"} />
+        <Input
+          placeholder="John"
+          label={"First Name"}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <Input
+          placeholder="Doe"
+          label={"Last Name"}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <Input
+          placeholder="abc@gmail.com"
+          label={"Email"}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          placeholder="123456"
+          label={"Password"}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <div className="pt-4">
-          <Button label={"Sign up"} />
+          <Button label={"Sign up"} onClick={onClick} />
         </div>
         <BottomWarning
           buttonText={"Login"}
